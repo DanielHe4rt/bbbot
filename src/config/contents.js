10913e203c2f1b521943f0f4d6d9a5041792c9be
gameszaum <gameszaum@gmail.com>
2020-03-31 11:31:17 -0300

    updated somethings and working

--- src/config/contents.js
+++ src/config/contents.js
@@ -34,7 +34,7 @@ const XPathContents = {
     '/html/body/div[1]/main/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div[1]/input',
   loginBtn:
     '/html/body/div[1]/main/div[2]/div/div/div/div[2]/div[1]/form/div[6]/button',
-  revoteBtn: '/html/body/div[2]/div[4]/div/div[3]/div/div/div[1]/div[2]/button',
+  revoteBtn: '/html/body/div[1]/div[4]/div/div[3]/div/div/div[1]/div[2]/button',
   captchaImage:
     '/html/body/div[2]/div[4]/div/div[1]/div[4]/div[2]/div[2]/div/div/div[2]/div/div[2]/img'
 };
